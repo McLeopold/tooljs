ca7e38e17ee2e565a5e45b64d6e1bff0d2371209
McLeopold <mcleopold@gmail.com>
2012-05-11 17:05:48 -0700

    Initial tooljs commit:
    The tooljs project includes utilities to make jake files look more like
    make files.  One of the goals is consistant behavior between linux, mac
    and windows.  Command line utilities that have differences between the
    systems will be duplicated.  The project will attempt to be as
    asynchronous as possible.
    
    The current features are:
    
    - finddir returns files that match wildard recusively
    
    - support for cp, rm, mkdir, rmdir, echo, exec
    
    - async with callbacks
    
    - chaining style creation of execution lists, which is gear for the
      nodejs async module

---
+++ go.js
@@ -0,0 +1,37 @@
+var finddir = require('./lib/util/finddir.js')
+  , tool = require('./lib/util/file.js')
+  , exec = require('./lib/util/exec.js')
+;
+
+/*
+finddir('node_modules/*',
+  { recursive: true,
+    dirs: true,
+    stats: true },
+  function (err, root, files) {
+    console.log('done');
+    files.forEach(function (file) {
+      console.log(root, file.name, file.stats.isFile());
+    })
+  }
+);
+*/
+
+/*
+tool.copy('node_modules', 'stuff', {recursive: true},
+  function (err) {
+    console.log('done with copy');
+  }
+);
+*/
+
+/*
+tool.rm('stuff', true)(function (err) {
+  console.log('done with remove');
+});
+*/
+
+exec([], {verbose: true})
+  .echo('test')
+  .copy('node_modules', 'stuff', {recursive: true})
+;
